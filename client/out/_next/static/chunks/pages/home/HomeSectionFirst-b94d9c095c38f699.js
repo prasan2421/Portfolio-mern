39296521d03533f5f2d331dbe058ac0d85c8dbc5
Prasanna Tuladhar <tuladharprasan@gmail.com>
2022-10-01 23:27:11 +0200

    Typescript changes

--- client/out/_next/static/chunks/pages/home/HomeSectionFirst-b94d9c095c38f699.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[68],{55930:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/home/HomeSectionFirst",function(){return u(60629)}])}},function(n){n.O(0,[72,282,941,896,30,774,888,179],(function(){return _=55930,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
